d14482bd4ac24f7f5a8704e638c86aa424daf152
Tajpuriyasuneel <075bct088@ioepc.edu.np>
2023-01-26 14:53:12 +0545

    v1 of react-countdown-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [timerDays, setTimerDays] = useState("00");
+  const [timerHours, setTimerHours] = useState("00");
+  const [timerMinutes, setTimerMinutes] = useState("00");
+  const [timerSeconds, setTimerSeconds] = useState("00");
+
+  function startTimer() {
+    const countdownDate = new Date("Jan 26, 2023 15:00:00").getTime();
+
+    setInterval(() => {
+      const now = new Date().getTime();
+      const remTime = countdownDate - now;
+
+      const days = Math.floor(remTime / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (remTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((remTime % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((remTime % (1000 * 60)) / 1000);
+      if (remTime > 0) {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      } else {
+        //if remTime < 0
+        setTimerDays("00");
+        setTimerHours("00");
+        setTimerMinutes("00");
+        setTimerSeconds("00");
+        clearInterval();
+      }
+    }, 1000);
+  }
+
+  startTimer();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="timer-container">
+      <section className="timer">
+        <div>
+          <span className="mdi mdi-calendar-clock timer-icon"></span>
+          <h2>Countdown Timer</h2>
+          <p>Countdown to a really special date. One you could need to use.</p>
+        </div>
+        <div>
+          <section>
+            <p>{timerDays}</p>
+            <p>
+              <small>Days</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerHours}</p>
+            <p>
+              <small>Hours</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerMinutes}</p>
+            <p>
+              <small>Minutes</small>
+            </p>
+          </section>
+          <span>:</span>
+          <section>
+            <p>{timerSeconds}</p>
+            <p>
+              <small>Seconds</small>
+            </p>
+          </section>
+        </div>
+      </section>
+    </section>
   );
 }
 
